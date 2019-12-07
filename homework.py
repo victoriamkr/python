dd41af540f6fafe1ff4747ed0e033482ed5cdb28
victoriamkr <victoria.mkr@gmail.com>
2019-12-07 17:26:36 +0400

    - homework 2, for and while loops

--- homework.py
+++ homework.py
@@ -1,3 +1,38 @@
 name = 'Victoria'
 surname = 'Mkrtchyan'
 print(name + surname)
+
+name = 'Victoria'
+surname = 'Mkrtchyan'
+print(name + surname)
+
+# Split string 'name' into characters
+def split(name):
+    return list(name)
+
+print(split(name))
+
+# Iterating using for loop
+for x in split(name):
+    print(x)
+
+# Split string 'surname' into characters
+def split(surname):
+    return list(surname)
+print(split(surname))
+
+# Getting length of list
+length = len(split(surname))
+i = 0
+
+# Iterating using while loop
+while i < length:
+    print(list(surname)[i])
+    i += 1
+
+# or second usage of while loop
+
+a = list(surname)
+while a:
+    print(a.pop(0))
+
